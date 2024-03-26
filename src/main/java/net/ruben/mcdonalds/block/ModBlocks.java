0da3b13ccafba9a6710e0de2e202172fdfcc1d17
smo0008 <smo0008@ringwoodsc.vic.edu.au>
2024-03-26 16:51:26 +1100

    adds blocks

--- src/main/java/net/ruben/mcdonalds/block/ModBlocks.java
+++ src/main/java/net/ruben/mcdonalds/block/ModBlocks.java
@@ -1,5 +1,6 @@
 package net.ruben.mcdonalds.block;
 
+import net.minecraft.item.Item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
