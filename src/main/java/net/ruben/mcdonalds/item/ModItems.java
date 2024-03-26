9a967a5ed61d7442708a9f7902d44903af5d4475
smo0008 <smo0008@ringwoodsc.vic.edu.au>
2024-03-26 12:08:20 +1100

    added size options for coke

--- src/main/java/net/ruben/mcdonalds/item/ModItems.java
+++ src/main/java/net/ruben/mcdonalds/item/ModItems.java
@@ -1,7 +1,7 @@
 package net.ruben.mcdonalds.item;
 
+import com.ibm.icu.util.CodePointTrie;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
@@ -15,16 +15,32 @@ public class ModItems {
     public static final Item MCNUGGET = registerItem("mcnugget", new Item(new FabricItemSettings().food(ModFoodComponents.MCNUGGET)));
     public static final Item CASH_REGISTER = registerItem("cash_register", new Item(new FabricItemSettings().food(ModFoodComponents.CASH_REGISTER)));
     public static final Item HASHBROWN = registerItem("hashbrown", new Item(new FabricItemSettings().food(ModFoodComponents.HASHBROWN)));
+    public static final Item SMALL_COKE = registerItem("small_coke", new SmallCokeItem(new FabricItemSettings().food(ModFoodComponents.SMALL_COKE)));
+    public static final Item MEDIUM_COKE = registerItem("medium_coke", new MediumCokeItem(new FabricItemSettings().food(ModFoodComponents.MEDIUM_COKE)));
     public static final Item LARGE_COKE = registerItem("large_coke", new LargeCokeItem(new FabricItemSettings().food(ModFoodComponents.LARGE_COKE)));
+
     public static final Item APPLE_PIE = registerItem("apple_pie", new Item(new FabricItemSettings().food(ModFoodComponents.APPLE_PIE)));
-    public static final Item MCPICKAXE = registerItem("mcpickaxe", new PickaxeItem(ModToolMaterial.MCNUGGET, 2, 2f, new FabricItemSettings()));
+    public static final Item MCPICKAXE = registerItem("mcpickaxe", new PickaxeItem(ModToolMaterial.MCNUGGET, 1, 0.5f, new FabricItemSettings()));
 
 
     public static void registerModItems() {
         McdonaldsMod.LOGGER.info("Registering Mod Items for " + McdonaldsMod.MOD_ID);
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::modifyEntries);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register((entries) -> {
             entries.add(MCPICKAXE);
+
+
+        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register((entries) -> {
+            entries.add(BIG_MAC);
+            entries.add(SMALL_FRIES);
+            entries.add(MCNUGGET);
+            entries.add(CASH_REGISTER);
+            entries.add(HASHBROWN);
+            entries.add(LARGE_COKE);
+            entries.add(APPLE_PIE);
+            entries.add(SMALL_COKE);
+            entries.add(MEDIUM_COKE);
+            entries.add(LARGE_COKE);
 
 
         });
@@ -33,15 +49,4 @@ public class ModItems {
         return Registry.register(Registries.ITEM, new Identifier(McdonaldsMod.MOD_ID, name), item);
     }
 
-    private static void modifyEntries(FabricItemGroupEntries entries) {
-        entries.add(BIG_MAC);
-        entries.add(SMALL_FRIES);
-        entries.add(MCNUGGET);
-        entries.add(CASH_REGISTER);
-        entries.add(HASHBROWN);
-        entries.add(LARGE_COKE);
-        entries.add(APPLE_PIE);
-
-
-    }
 }
