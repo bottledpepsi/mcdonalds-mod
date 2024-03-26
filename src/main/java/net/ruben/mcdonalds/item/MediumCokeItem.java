d314a35d525bbebdae76724245d075ea92f0baee
smo0008 <smo0008@ringwoodsc.vic.edu.au>
2024-03-26 12:08:29 +1100

    added size options for coke

---
+++ src/main/java/net/ruben/mcdonalds/item/MediumCokeItem.java
@@ -0,0 +1,33 @@
+package net.ruben.mcdonalds.item;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+public class MediumCokeItem extends Item {
+    public MediumCokeItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.DRINK;
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if (user instanceof PlayerEntity player) {
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 100, 0));
+            if (!player.getAbilities().creativeMode) {
+                stack.decrement(1);
+            }
+
+        }
+        return stack;
+    }
+}
